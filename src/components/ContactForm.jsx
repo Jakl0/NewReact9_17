d55e5cc525fc3dc64bcf14534e5f9c940165e58b
4M <4M@415-16>
2026-05-29 10:25:19 +0200

    commit

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,12 +5,19 @@ function ContactForm(){
 
     const [text1 ,settext1] = useState();
     const [text2 ,settext2] = useState();
+    const [message , setMessage] = useState();
+
+    function handleSubmit(event){
+        
+        
+        event.PreventDefault();
+    }
     return(
         <>
-        <form>
-            <input type="text" name="mail" className="form-control" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="mail"  className="form-control" />
             <textarea name="message" className="form-control" />
-            <button type="submit" className="btn btn-primary" ></button>
+            <button type="submit" className="btn btn-primary" >Wyślij</button>
             </form>
         </>
     )
